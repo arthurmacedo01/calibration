ac1632e055163ddf78a1caa2cd21b23a499bc8cc
Arthur_Macedo <arthurmacedo01@gmail.com>
2023-09-06 14:42:37 -0300

    Service

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_173247) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_173756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_173247) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "services", force: :cascade do |t|
+    t.text "description"
+    t.bigint "section_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_services_on_section_id"
+  end
+
+  add_foreign_key "services", "sections"
 end
