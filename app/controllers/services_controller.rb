0c5be03fd551a10f1dc40b11d81c544c7029760c
Arthur_Macedo <arthurmacedo01@gmail.com>
2023-09-19 10:34:32 -0300

    service includes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,7 +5,7 @@ class ServicesController < ApplicationController
   
   # GET /services or /services.json
   def index
-    @services = Service.all
+    @services = Service.includes(:section).all
   end
 
   # GET /services/1 or /services/1.json
